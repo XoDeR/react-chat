23389571385f091c541a4ae87bda1a1cd207bb8d
Volodymyr <XoDeR@users.noreply.github.com>
2024-08-03 20:35:02 +0200

    adding firebase app

---
+++ src/appFireBase/Config.ts
@@ -0,0 +1,37 @@
+import { initializeApp } from "firebase/app";
+import {
+  browserLocalPersistence,
+  getAuth,
+  setPersistence,
+} from "firebase/auth";
+import { getDatabase } from "firebase/database";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const {
+  VITE_API_KEY,
+  VITE_AUTH_DOMAIN,
+  VITE_PROJECT_ID,
+  VITE_STORAGE_BUCKET,
+  VITE_MESSAGING_SENDER_ID,
+  VITE_APP_ID,
+} = import.meta.env || ({} as NodeJS.Process["env"]);
+
+const firebaseConfig = {
+  apiKey: VITE_API_KEY,
+  authDomain: VITE_AUTH_DOMAIN,
+  projectId: VITE_PROJECT_ID,
+  storageBucket: VITE_STORAGE_BUCKET,
+  messagingSenderId: VITE_MESSAGING_SENDER_ID,
+  appId: VITE_APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app, `gs://${VITE_STORAGE_BUCKET}`);
+
+setPersistence(auth, browserLocalPersistence);
+
+export { auth, db, storage };
