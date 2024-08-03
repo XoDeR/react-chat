b225fb4bb376955935ffa8649c86b97ad6b4fb01
Volodymyr <XoDeR@users.noreply.github.com>
2024-08-03 23:07:55 +0200

    added interface for auth state

---
+++ src/interfaces/store/IAuthSliceState.ts
@@ -0,0 +1,11 @@
+import type { User } from "firebase/auth";
+
+export interface IAuthSliceState {
+  isLoggedIn: boolean | null;
+  currentUser: {
+    uid: string | null;
+    displayName: string | null;
+    photoURL: string | null;
+  };
+  updateCurrentUser: (user: User | null) => void;
+}
