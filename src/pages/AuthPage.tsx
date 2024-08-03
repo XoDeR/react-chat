9364f566fb456e20360822765250297ab6f0251a
Volodymyr <XoDeR@users.noreply.github.com>
2024-08-03 19:13:06 +0200

    auth page

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -1,5 +1,16 @@
 const AuthPage = () => {
-  return <main>Auth page</main>;
+  const step = "Step 1/3";
+
+  return (
+    <div className="relative flex h-full flex-col gap-2 bg-slate-400 py-10">
+      <div className="mx-auto h-full max-w-300px rounded-md bg-slate-700 text-white px-6">
+        CHAT
+      </div>
+      <p className="text-center font-bold text-white">
+        {"Step"} {step.split(" ")[1]}
+      </p>
+    </div>
+  );
 };
 
 export default AuthPage;
