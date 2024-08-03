34bc2090b8298f240a7d87c2258d31013b0b70f7
Volodymyr <XoDeR@users.noreply.github.com>
2024-08-03 15:44:17 +0200

    fixed aliases

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,3 +1,10 @@
+import { lazy } from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const Auth = lazy(() => import("@pages/AuthPage"));
+const HomePage = lazy(() => import("@pages/HomePage"));
+const ChatPage = lazy(() => import("@pages/ChatPage"));
+
 const AppRouter = () => {
   return <>App router</>;
 };
