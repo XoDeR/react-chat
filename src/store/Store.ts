9c6844fc5d5be7aa9f71ce1f4af2ca8d8dfba6c7
Volodymyr <XoDeR@users.noreply.github.com>
2024-08-03 23:14:11 +0200

    created zustand store

--- src/store/Store.ts
+++ src/store/Store.ts
@@ -1,2 +1,14 @@
 import { create } from "zustand";
 import { devtools } from "zustand/middleware";
+
+import createAuthSliceState from "./AuthSlice";
+
+import type { IAuthSliceState } from "@interfaces/store/IAuthSliceState";
+
+const useChatStore = create<IAuthSliceState>()(
+  devtools((...a) => ({
+    ...createAuthSliceState(...a),
+  }))
+);
+
+export default useChatStore;
